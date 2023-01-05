e574d5686d30fd2356a26b107446f2a8e31395d1
1598369123@qq.com <1598369123@qq.com>
2023-01-05 09:40:04 +0800

    conffig

--- src/config/alasite.php
+++ src/config/alasite.php
@@ -6,8 +6,8 @@
  * Email: <sr_yes@foxmail.com>
  */
 return [
-    'asset_url' => 'https://smse.sjtu.edu.cn',
-    'site_url'  => 'https://smse.sjtu.edu.cn',
-    'site_label' => 'en',
-    'banner_folder_id' => 11
+    'asset_url' => env('ALASITE_ASSET_URL','https://smse.sjtu.edu.cn'),
+    'site_url'  => env('ALASITE_SITE_URL','https://smse.sjtu.edu.cn'),
+    'site_label' => env('ALASITE_SITE_LABEL','en'),
+    'banner_folder_id' => env('ALASITE_BANNER_FOLDER_ID',11),
 ];
